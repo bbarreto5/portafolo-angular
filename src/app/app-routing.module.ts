f5f1369897e6180c954d8172db08e189cb3268f5
brandon <diegodjsonic@gmail.com>
2020-02-09 22:22:12 -0500

    page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'portafolio', component: PortafolioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
